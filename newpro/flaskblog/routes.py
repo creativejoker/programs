78f555edba17cf39acf5aa024d344364ff2305d9
creativejoker <41733926+creativejoker@users.noreply.github.com>
2018-09-16 11:29:34 +0530

    Add files via upload

---
+++ newpro/flaskblog/routes.py
@@ -0,0 +1,122 @@
+from flask import render_template, url_for, flash, redirect, request
+from flaskblog import app, db, bcrypt, mail
+from flaskblog.forms import RegistrationForm, LoginForm,RequestResetForm,ResetPasswordForm
+from flaskblog.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
+from flask_socketio import SocketIO, emit
+from flask_mail import Message
+
+socketio = SocketIO( app )
+
+
+
+
+@app.route("/")
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    request_data = request.form
+    if current_user.is_authenticated:
+        return redirect(url_for('login'))
+    form = RegistrationForm()
+    print(form)
+
+    #if frequest_data.validate_on_submit():
+    if request_data:
+       
+        print('\n',request_data['specification'],'\n')
+        hashed_password = bcrypt.generate_password_hash(request_data['password']).decode('utf-8')
+        user = User(username=request_data['username'], email=request_data['email'], password=hashed_password,specification=request_data['specification'])
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('account'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('account'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title='Account')
+
+
+@app.route( '/ChatApp' )
+def ChatApp():
+  return render_template( './ChatApp.html' )
+
+def messageRecived():
+  print( 'message was received!!!' )
+
+
+@socketio.on( 'my event' )
+def handle_my_custom_event( json ):
+    print( 'recived my event: ' + str( json ) )
+    socketio.emit( 'my response', json, callback=messageRecived )
+
+
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+    msg = Message('Password Reset Request',
+                  sender='noreply@demo.com',
+                  recipients=[user.email])
+    msg.body = f'''To reset your password, visit the following link:
+{url_for('reset_token', token=token, _external=True)}
+If you did not make this request then simply ignore this email and no changes will be made.
+'''
+    mail.send(msg)
+
+
+
+@app.route("/reset_password", methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('account'))
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        send_reset_email(user)
+        flash('An email has been sent with instructions to reset your password.', 'info')
+        return redirect(url_for('login'))
+    return render_template('reset_request.html', title='Reset Password', form=form)
+
+
+@app.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('account'))
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash('That is an invalid or expired token', 'warning')
+        return redirect(url_for('reset_request'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user.password = hashed_password
+        db.session.commit()
+        flash('Your password has been updated! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('reset_token.html', title='Reset Password', form=form)
+
+  
