50e51e4ee6d7a5dabdd9ecbc3b31eebd677f1c22
creativejoker <41733926+creativejoker@users.noreply.github.com>
2018-09-16 12:55:53 +0530

    Add files via upload

--- newpro/flaskblog/routes.py
+++ newpro/flaskblog/routes.py
@@ -54,11 +54,25 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
-
-@app.route("/account")
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account',
+                           image_file=image_file, form=form)
 
 
 @app.route( '/ChatApp' )
