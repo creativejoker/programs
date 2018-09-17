fd7540309e51d28986aeb9da65b6ed242d06318d
creativejoker <41733926+creativejoker@users.noreply.github.com>
2018-09-17 18:24:35 +0530

    Add files via upload

---
+++ pro/main.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+from flask import Flask, render_template, url_for, flash, redirect
+
+app = Flask(__name__)
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
